979f36e80074823b6547848639b43e57042e60f8
Vladimir Kostetskyi <nobergann@gmail.com>
2021-07-19 12:14:53 +0300

    release

--- src/js/video-play.js
+++ src/js/video-play.js
@@ -1,68 +1,68 @@
-// const videoRef = document.querySelector('#myVideo');
-// const playBtnRef = document.querySelector('.play-btn__img');
-// const playBtnDesktopRef = document.querySelector('.play-btn__img-desktop');
-
-// playBtnRef.addEventListener('click', playVid);
-// playBtnDesktopRef.addEventListener('click', playVidDesktop);
-
-// function playVidDesktop() {
-//   videoRef.play();
-//   playBtnDesktopRef.classList.add('is-hidden');
-//   videoRef.controls = 'controls';
-// }
-
-// function playVid() {
-//   videoRef.play();
-//   playBtnRef.classList.add('is-hidden');
-//   videoRef.controls = 'controls';
-// }
-
 const videoRef = document.querySelector('#myVideo');
-
-// Mob
 const playBtnRef = document.querySelector('.play-btn__img');
-
-playBtnRef.addEventListener('click', handlePlayButtonMob, false);
-// playVideoMob();
-
-async function playVideoMob() {
-  try {
-    await videoRef.play();
-    playBtnRef.classList.add('is-hidden');
-    videoRef.controls = 'controls';
-  } catch (err) {
-    playBtnRef.classList.remove('playing');
-  }
-}
-
-function handlePlayButtonMob() {
-  if (videoRef.paused) {
-    playVideoMob();
-  } else {
-    videoRef.pause();
-  }
-}
-
-// Desk
 const playBtnDesktopRef = document.querySelector('.play-btn__img-desktop');
 
-playBtnDesktopRef.addEventListener('click', handlePlayButton, false);
-// playVideo();
+playBtnRef.addEventListener('click', playVid);
+playBtnDesktopRef.addEventListener('click', playVidDesktop);
 
-async function playVideo() {
-  try {
-    await videoRef.play();
-    playBtnDesktopRef.classList.add('is-hidden');
-    videoRef.controls = 'controls';
-  } catch (err) {
-    playBtnDesktopRef.classList.remove('playing');
-  }
+function playVidDesktop() {
+  videoRef.play();
+  playBtnDesktopRef.classList.add('is-hidden');
+  videoRef.controls = 'controls';
 }
 
-function handlePlayButton() {
-  if (videoRef.paused) {
-    playVideo();
-  } else {
-    videoRef.pause();
-  }
+function playVid() {
+  videoRef.play();
+  playBtnRef.classList.add('is-hidden');
+  videoRef.controls = 'controls';
 }
+
+// const videoRef = document.querySelector('#myVideo');
+
+// // Mob
+// const playBtnRef = document.querySelector('.play-btn__img');
+
+// playBtnRef.addEventListener('click', handlePlayButtonMob, false);
+// // playVideoMob();
+
+// async function playVideoMob() {
+//   try {
+//     await videoRef.play();
+//     playBtnRef.classList.add('is-hidden');
+//     videoRef.controls = 'controls';
+//   } catch (err) {
+//     playBtnRef.classList.remove('playing');
+//   }
+// }
+
+// function handlePlayButtonMob() {
+//   if (videoRef.paused) {
+//     playVideoMob();
+//   } else {
+//     videoRef.pause();
+//   }
+// }
+
+// // Desk
+// const playBtnDesktopRef = document.querySelector('.play-btn__img-desktop');
+
+// playBtnDesktopRef.addEventListener('click', handlePlayButton, false);
+// // playVideo();
+
+// async function playVideo() {
+//   try {
+//     await videoRef.play();
+//     playBtnDesktopRef.classList.add('is-hidden');
+//     videoRef.controls = 'controls';
+//   } catch (err) {
+//     playBtnDesktopRef.classList.remove('playing');
+//   }
+// }
+
+// function handlePlayButton() {
+//   if (videoRef.paused) {
+//     playVideo();
+//   } else {
+//     videoRef.pause();
+//   }
+// }
